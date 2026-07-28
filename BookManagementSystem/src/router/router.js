9cd45f1e634580f8c79a70f023613220cf878271
wuwang660 <1837204337@qq.com>
2026-07-28 11:45:13 +0800

    修改路由模式

--- BookManagementSystem/src/router/router.js
+++ BookManagementSystem/src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import BookManagement from "../components/BookManagement.vue";
 import Login from "../components/Login.vue";
 import { authState } from "../composables/useAuth.js";
@@ -14,7 +14,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
 });
 
